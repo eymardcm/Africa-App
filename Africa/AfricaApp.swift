269bd8a6e80df19be9c3533d1bccc58ccd37a356
Chad Eymard <eymardcm@gmail.com>
2024-03-06 21:43:09 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Chad Eymard on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
