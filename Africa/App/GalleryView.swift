8922c6d793b8e9b58c99c15e0b67568b2d62b2a1
Chad Eymard <eymardcm@gmail.com>
2024-03-10 13:35:37 -0400

    gallery grid layout and showing images

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -10,11 +10,41 @@ import SwiftUI
 struct GalleryView: View {
     
     // MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // Simple grid definition
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
     
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack (alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(.white, lineWidth: 8)
+                    )
+                // MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    } //: LOOP
+                } //: GRID
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
